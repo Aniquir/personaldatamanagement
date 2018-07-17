9de236573968105c0be272d21ce8151bac0b84c8
Aniquir <pawel9658@wp.pl>
2018-07-17 12:25:09 +0200

    initial commit

---
+++ src/main/java/com/example/personaldatamanagement/PersonaldatamanagementApplication.java
@@ -0,0 +1,12 @@
+package com.example.personaldatamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaldatamanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaldatamanagementApplication.class, args);
+    }
+}
